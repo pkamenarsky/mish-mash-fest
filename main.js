5a05f94351d805419cc8231ef8395dde6a0f17dc
Philip Kamenarsky <p.kamenarsky@gmail.com>
2019-03-07 10:38:45 +0000

    Adjustments

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ setDisplay(visible_layer, 'unset');
 
 window.addEventListener('deviceorientation', function(event) {
   setDisplay(visible_layer, 'none');
-  visible_layer = event.beta * 3;
+  visible_layer = Math.round((event.beta - 50) / -2) + 3;
   visible_layer = Math.min(7, Math.max(0, visible_layer));
   setDisplay(visible_layer, 'unset');
 
-  const e = document.getElementById('beta');
-  e.innerHTML = event.beta + ', ' + visible_layer;
+  // const e = document.getElementById('beta');
+  // e.innerHTML = event.beta + ', ' + visible_layer;
 });
