b870f9973b09716d049d2f4b1aa08d376ba37456
Philip Kamenarsky <p.kamenarsky@gmail.com>
2019-03-11 19:26:48 +0000

    WIP

--- main.js
+++ main.js
@@ -23,17 +23,84 @@ function updateLayers() {
 
 // updateLayers();
 
-window.addEventListener('deviceorientation', function(event) {
-  // visible_layer = Math.round((event.beta - 50) / -2) + 3;
-  // visible_layer = Math.min(7, Math.max(0, visible_layer));
-  // updateLayers();
+function displayLayers() {
+  const e = document.getElementById('layers');
+  let str = '';
 
-  let e = document.getElementById('alpha');
-  e.innerHTML = event.alpha;
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 4; x++) {
+      str += visible_layers[x + y * 4] + ' ';
+    }
 
-  e = document.getElementById('beta');
-  e.innerHTML = event.beta;
+    str += '<br>';
+  }
 
-  e = document.getElementById('gamma');
-  e.innerHTML = event.gamma;
-});
+  e.innerHTML = str;
+}
+
+function displayLayersConsole() {
+  for (let y = 0; y < 3; y++) {
+    let str = '';
+    for (let x = 0; x < 4; x++) {
+      str += visible_layers[x + y * 4] + ' ';
+    }
+
+    console.log(str);
+  }
+}
+
+function computeLayers(posX, posY) {
+  const tl = 0;
+  const tr = 0;
+  const bl = 0;
+  const br = 0;
+
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 4; x++) {
+      const nX = x / 3.0, nY = y / 2.0;
+      const dX = posX - nX, dY = posY - nY;
+      const d = dX * dX + dY * dY;
+
+      // 0.5 = everything 3
+      // 1 = right 5, left 1
+      // 0 = left 5, right 1
+      visible_layers[x + y * 4] = d;
+      // visible_layers[x + y * 4] = Math.round(d * 100);
+      // visible_layers[x + y * 4] = Math.round(nX * 10) + ':' + Math.round(nY * 10);
+    }
+  }
+}
+
+function run() {
+  window.addEventListener('deviceorientation', function(event) {
+    // visible_layer = Math.round((event.beta - 50) / -2) + 3;
+    // visible_layer = Math.min(7, Math.max(0, visible_layer));
+    // updateLayers();
+
+    const posX = (Math.max(-10, Math.min(10, event.gamma)) + 10) / 20;
+    const posY = (Math.max(20, Math.min(50, event.beta)) - 20) / 30;
+
+    computeLayers(posX, posY);
+    displayLayers();
+
+    let e = document.getElementById('posx');
+    e.innerHTML = posX;
+
+    e = document.getElementById('posy');
+    e.innerHTML = posY;
+
+    // let e = document.getElementById('alpha');
+    // e.innerHTML = event.alpha;
+
+    // 20 - 50
+    // e = document.getElementById('beta');
+    // e.innerHTML = event.beta;
+
+    // -20 - 20
+    // e = document.getElementById('gamma');
+    // e.innerHTML = event.gamma;
+  });
+}
+
+computeLayers(0.5, 0.5);
+displayLayersConsole();
